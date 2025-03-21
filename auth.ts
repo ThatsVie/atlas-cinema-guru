ce461b81434c0c9b80f4594b52255d15a5076bd6
Thatsvie <viepaulalmt@gmail.com>
2025-03-21 15:55:37 -0500

    cinema-guru-pug

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import GitHubProvider from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
     brandColor: "#1ED2AF",
-    logo: "/logo.png",
+    logo: "/cinema-guru-pug.jpg",
     buttonText: "#ffffff",
   },
   providers: [
